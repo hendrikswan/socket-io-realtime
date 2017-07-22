b739eb82edd70ed002f3976ea863f2ea9f384ee0
hendrik swanepoel <hendrik.swanepoel@gmail.com>
2017-07-22 16:42:30 +0200

    feeding data over sockets through RethinkDB

--- server/index.js
+++ server/index.js
@@ -1,13 +1,31 @@
+const r = require('rethinkdb');
 const io = require('socket.io')();
 
-io.on('connection', (client) => {
-  client.on('subscribeToTimer', (interval) => {
-    console.log('client is subscribing to timer with interval ', interval);
-    setInterval(() => {
-      client.emit('timer', new Date());
-    }, interval);
+
+r.connect({
+  host: 'localhost',
+  port: 28015,
+  db: 'awesome_whiteboard'
+}).then((connection) => {
+  io.on('connection', (client) => {
+    client.on('subscribeToTimer', (interval) => {
+      // console.log('client is subscribing to timer with interval ', interval);
+      // setInterval(() => {
+      //   client.emit('timer', new Date());
+      // }, interval);
+      return r.table('timers')
+      .changes()
+      .run(connection)
+      .then((cursor) => {
+        cursor.each((err, timerRow) => {
+          client.emit('timer', timerRow.new_val.timestamp);
+        });
+      });
+    });
+
   });
 });
+
 
 const port = 8000;
 io.listen(port);
